58d329bc6aeb627ef6dade2d7997ff855280cdb7
Charuka Crishani <fcharuka18@gmail.com>
2026-04-30 10:35:26 +0530

    fix env variables

--- backend/utils/token_creator.py
+++ backend/utils/token_creator.py
@@ -1,15 +1,28 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+from dotenv import load_dotenv
+from google_auth_oauthlib.flow import Flow
+
+load_dotenv()
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-flow = InstalledAppFlow.from_client_secrets_file(
-    "../credentials.json", SCOPES
+flow = Flow.from_client_config(
+    {
+        "installed": {
+            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+            "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "redirect_uris": [os.getenv("GOOGLE_REDIRECT_URI")],
+        }
+    },
+    scopes=SCOPES,
 )
 
 creds = flow.run_local_server(
     port=0,
-    access_type="offline",   # REQUIRED
-    prompt="consent"         # ensures refresh token
+    access_type="offline",
+    prompt="consent"
 )
 
 with open("../token.json", "w") as token:
