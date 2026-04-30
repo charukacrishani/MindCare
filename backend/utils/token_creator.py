9eb48cef4f6688bbb54a9d21fb1d9f36cde95600
Charuka Crishani <fcharuka18@gmail.com>
2026-04-30 10:42:46 +0530

    fix token creator

--- backend/utils/token_creator.py
+++ backend/utils/token_creator.py
@@ -1,19 +1,21 @@
 import os
 from dotenv import load_dotenv
-from google_auth_oauthlib.flow import Flow
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 load_dotenv()
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-flow = Flow.from_client_config(
+flow = InstalledAppFlow.from_client_config(
     {
         "installed": {
-            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-            "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+            "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+            "project_id": os.environ.get("GOOGLE_PROJECT_ID"),
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
-            "redirect_uris": [os.getenv("GOOGLE_REDIRECT_URI")],
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_secret": os.environ.get("GOOGLE_CLIENT_SECRET"),
+            "redirect_uris": ["http://localhost"],
         }
     },
     scopes=SCOPES,
@@ -28,4 +30,4 @@ creds = flow.run_local_server(
 with open("../token.json", "w") as token:
     token.write(creds.to_json())
 
-print("✅ token.json generated")
+print("token.json generated")
